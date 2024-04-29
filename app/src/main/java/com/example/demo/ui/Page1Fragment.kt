0bc7a7dcbc257ff5c2fb8cbae5b0f05d3ef6ae10
Ruefox123 <logintoauniverse@protonmail.com>
2024-04-29 14:48:02 +0800

    Test1 commit

---
+++ app/src/main/java/com/example/demo/ui/Page1Fragment.kt
@@ -0,0 +1,29 @@
+package com.example.demo.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.demo.R
+import com.example.demo.databinding.FragmentPage1Binding
+
+class Page1Fragment : Fragment() {
+
+    private lateinit var binding: FragmentPage1Binding
+    private val nav by lazy { findNavController() }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentPage1Binding.inflate(inflater, container, false)
+
+        binding.btn.setOnClickListener {
+            // TODO: Clear back stack (history)
+            nav.popBackStack(R.id.homeFragment, false)
+            nav.navigate(R.id.page2Fragment)
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/demo/ui/PageBFragment.kt
@@ -0,0 +1,26 @@
+package com.example.demo.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.demo.databinding.FragmentPageBBinding
+
+class PageBFragment : Fragment() {
+
+    private lateinit var binding: FragmentPageBBinding
+    private val nav by lazy { findNavController() }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentPageBBinding.inflate(inflater, container, false)
+
+        binding.btn.setOnClickListener {
+            nav.navigateUp()
+        }
+
+        return binding.root
+    }
+
+}
